01e834abee1d4bcbf85aebe584b0f9b5f819c022
sainath <sainath@DESKTOP-F50PPG2>
2023-12-11 18:39:42 +0530

    00000

---
+++ Object Repository/RBT/Footer page objects/Page_AI, Dynamics 365 ERP  CRM, Kentico, Si_bac1b0/a_1 650 670 7605.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_1 650 670 7605</name>
+   <tag></tag>
+   <elementGuidId>ae12839e-8139-4bde-a654-fbe612b99c06</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body[@id='post']/footer/div/div/div/div[3]/div/div/div/ul/li[2]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ce00a330-2509-4f45-baea-6c195b2497ea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>tel:+1 650 670 7605</value>
+      <webElementGuid>05ee163c-149b-4dbf-9a30-1cbe6a83ce71</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>+1 650 670 7605</value>
+      <webElementGuid>16bb4cae-a99d-48a1-815a-46db0e246710</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post&quot;)/footer[1]/div[@class=&quot;primary-footer&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row wow animated&quot;]/div[@class=&quot;col-sm-12 col-md-3&quot;]/div[@class=&quot;footer-links contact-links&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;mb-20 col-sm-6 col-md-12&quot;]/ul[1]/li[2]/a[1]</value>
+      <webElementGuid>481cbab5-c7b4-4071-b3a6-cb4fdb74aab5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//body[@id='post']/footer/div/div/div/div[3]/div/div/div/ul/li[2]/a</value>
+      <webElementGuid>460dc5f3-29b8-454c-ba47-17ad70094b6b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'+1 650 670 7605')]</value>
+      <webElementGuid>0a011b5d-fe98-45e5-ae51-fc35515f5d9c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='sales@raybiztech.com'])[1]/following::a[1]</value>
+      <webElementGuid>a5a90c4d-d9b4-46a1-a54f-0a44d0778929</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Quick Contact'])[1]/following::a[2]</value>
+      <webElementGuid>4fcc4b56-600b-4c9e-8830-31088394d500</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Social Media'])[1]/preceding::a[1]</value>
+      <webElementGuid>34c8bcb9-cde8-4d88-96e3-19121145149e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cookie Policy'])[1]/preceding::a[6]</value>
+      <webElementGuid>50647931-7a40-4a4e-9d88-26c8e9ba9a11</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='+1 650 670 7605']/parent::*</value>
+      <webElementGuid>e30491e5-bb32-4627-9527-1d513e297d4d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'tel:+1 650 670 7605')]</value>
+      <webElementGuid>965eafba-4f01-4b40-8d54-40a1cd2d90b5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div/ul/li[2]/a</value>
+      <webElementGuid>4a02dd1c-a7af-4c70-8bbc-57c12bf4e63e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'tel:+1 650 670 7605' and (text() = '+1 650 670 7605' or . = '+1 650 670 7605')]</value>
+      <webElementGuid>ea44f30a-70e5-4a10-a518-e614ac14b8b5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
