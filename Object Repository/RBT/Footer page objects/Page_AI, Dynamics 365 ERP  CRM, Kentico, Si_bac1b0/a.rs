eedab9730cbe2519de6bc072e5a6a5748f2afa93
sainath <sainath@DESKTOP-F50PPG2>
2023-12-11 18:39:19 +0530

    l;

---
+++ Object Repository/RBT/Footer page objects/Page_AI, Dynamics 365 ERP  CRM, Kentico, Si_bac1b0/a.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a</name>
+   <tag></tag>
+   <elementGuidId>df6b3ca7-6083-48fb-b1e7-1fb47f5913d8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body[@id='post']/footer/div/div/div/div[3]/div/div/div[2]/ul/li/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.mb-20.col-sm-6.col-md-12 > ul.social-media > li.facebook > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>df1d708c-2672-4c3e-bc18-1b7f3ebf49ce</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.facebook.com/RBTOriginals</value>
+      <webElementGuid>1e9d12e0-3265-4e95-b724-6c0f8db2a18b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+      <webElementGuid>4103ac88-7b06-46c7-95a6-4be28ffa475e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post&quot;)/footer[1]/div[@class=&quot;primary-footer&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row wow animated&quot;]/div[@class=&quot;col-sm-12 col-md-3&quot;]/div[@class=&quot;footer-links contact-links&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;mb-20 col-sm-6 col-md-12&quot;]/ul[@class=&quot;social-media&quot;]/li[@class=&quot;facebook&quot;]/a[1]</value>
+      <webElementGuid>1465eb3a-b4cb-4b47-bb31-9d337cc07329</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//body[@id='post']/footer/div/div/div/div[3]/div/div/div[2]/ul/li/a</value>
+      <webElementGuid>259c0311-c709-4ead-9a7d-3604a23cf518</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Social Media'])[1]/following::a[1]</value>
+      <webElementGuid>d6736cc8-5537-461e-aebc-d70069641564</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='sales@raybiztech.com'])[1]/following::a[2]</value>
+      <webElementGuid>feef7c27-e03b-41b8-8fd0-9c75b593e603</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cookie Policy'])[1]/preceding::a[5]</value>
+      <webElementGuid>a2a3cca8-0606-4364-a307-ade6b2c9e7f1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://www.facebook.com/RBTOriginals')]</value>
+      <webElementGuid>53660162-6e73-44a7-8539-00e2fb889308</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div[2]/ul/li/a</value>
+      <webElementGuid>74176c38-090b-4dbc-8e78-28cc628be9b5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://www.facebook.com/RBTOriginals']</value>
+      <webElementGuid>70544227-701a-4867-8976-4fa9c8ef79b4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
